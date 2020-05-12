5afcc60052730885de1b518b4649ed41dcd309c8
slavabily <slavabily@me.com>
2020-05-12 18:30:48 +0300

    Initial Commit

---
+++ VoiceOver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VoiceOver
+//
+//  Created by slava bily on 12/5/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

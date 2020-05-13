8cc006bba48c1de96f37db11c7d745ed6c6a682d
slavabily <slavabily@me.com>
2020-05-13 18:50:38 +0300

    Reading the value of controls

--- VoiceOver/ContentView.swift
+++ VoiceOver/ContentView.swift
@@ -25,18 +25,37 @@ struct ContentView: View {
     ]
     
     @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var estimate = 25.0
+    @State private var rating = 3
     
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .accessibility(label: Text(labels[selectedPicture]))
-            .accessibility(addTraits: .isButton)
-            .accessibility(removeTraits: .isImage)
-            .onTapGesture {
-                self.selectedPicture = Int.random(in: 0...3)
-        }
+        //        Image(pictures[selectedPicture])
+        //            .resizable()
+        //            .scaledToFit()
+        //            .accessibility(label: Text(labels[selectedPicture]))
+        //            .accessibility(addTraits: .isButton)
+        //            .accessibility(removeTraits: .isImage)
+        //            .onTapGesture {
+        //                self.selectedPicture = Int.random(in: 0...3)
+        //        }
         
+//        Image(decorative: "ales-krivec-15949")
+//            .accessibility(hidden: true)
+        
+//        VStack {
+//            Text("Your score is")
+//            Text("1000")
+//                .font(.title)
+//        }
+//        .accessibilityElement(children: .combine)
+//        .accessibility(label: Text("Your score is 1000"))
+        
+//        Slider(value: $estimate, in: 0...50)
+//        .padding()
+//        .accessibility(value: Text("\(estimate)"))
+        
+        Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+        .accessibility(value: Text("\(rating) out of 5"))
     }
 }
 
